1664316f2e12d0a20fcd54a6ff997c535e703b75
alexander-romanov-V <romanovav@ya.ru>
2025-03-20 22:58:34 +0300

    1.3

---
+++ 1lab/bank.py
@@ -0,0 +1,11 @@
+def main():
+    greetings = input("Greetings: ").strip().lower()
+
+    if greetings.startswith("hello"):
+        print("$0")
+    elif greetings.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
+main()
---
+++ 1lab/interpreter.py
@@ -0,0 +1,24 @@
+def main():
+    expresssion = input("Expression: ")
+    x, y, z = expresssion.split(" ")
+    x = float(x)
+    z = float(z)
+    res = 0
+    match y:
+        case "+":
+            res = x + z
+        case "-":
+            res = x - z
+        case "*":
+            res = x * z
+        case "/":
+            if z == 0:
+                print("Can't divide on 0")
+                return
+            res = x / z
+        case _:
+            print("Wrong math symbol")
+
+    print(f"{res:.1f}")
+
+main()
