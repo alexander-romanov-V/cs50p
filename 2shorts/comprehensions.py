bfa5db6e0bd7ca110bacf002163b4f0b02f461fd
alexander-romanov-V <romanovav@ya.ru>
2025-03-23 18:30:25 +0300

    2.10

--- 2shorts/comprehensions.py
+++ 2shorts/comprehensions.py
@@ -3,7 +3,7 @@ import re
 
 
 def main():
-    words = get_words("2shorts/address.txt")
+    words = get_words("address.txt")
     lowercase_words = [word.lower() for word in words if len(word) > 4]
 
     counts = {word: lowercase_words.count(word) for word in lowercase_words}
