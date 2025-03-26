2efd8c7a22bad6baf627b4dc78e848088da28f54
alexander-romanov-V <romanovav@ya.ru>
2025-03-26 14:46:24 +0300

    3.6

---
+++ 3labs/fuel.py
@@ -0,0 +1,27 @@
+def main():
+    while True:
+        try:
+            x, y = input("Fraction: ").split("/")
+            f = convert(x, y)
+        except (ValueError, ZeroDivisionError):
+            pass
+        else:
+            break
+    if f <= 1:
+        print("E")
+    elif f >= 99:
+        print("F")
+    else:
+        print(f"{f}%")
+
+
+def convert(x, y):
+    x = int(x)
+    y = int(y)
+    if x > y or y == 0:
+        raise ValueError
+    return int(100 * x / y)
+
+
+if __name__ == "__main__":
+    main()
