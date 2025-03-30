4b9b11ef75f47e9b01165ddf211c4baf918b9231
alexander-romanov-V <romanovav@ya.ru>
2025-03-30 19:09:02 +0300

    4.12

---
+++ 4labs/figlet.py
@@ -0,0 +1,30 @@
+"""Work with system, random and figlet"""
+
+import sys
+import random
+import pyfiglet
+
+
+def main():
+    """Main code"""
+    figlet = pyfiglet.Figlet()
+    fonts = figlet.getFonts()
+
+    if len(sys.argv) == 1:
+        fnt = random.choice(fonts)
+    elif (
+        len(sys.argv) == 3
+        and (sys.argv[1] == "-f" or sys.argv[1] == "--font")
+        and sys.argv[2] in fonts
+    ):
+        fnt = sys.argv[2]
+    else:
+        sys.exit("Invalid usage")
+
+    txt = input("Input: ")
+    figlet.setFont(font=fnt)
+    print(figlet.renderText(txt))
+
+
+if __name__ == "__main__":
+    main()
