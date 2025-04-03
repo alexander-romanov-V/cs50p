a992be41afadd0cec078721fa5fc711c58d9e70b
alexander-romanov-V <romanovav@ya.ru>
2025-04-03 12:23:23 +0300

    5.13

--- 6lecture/students6.py
+++ 6lecture/students6.py
@@ -5,10 +5,13 @@ students = []
 with open("students6.csv", encoding="utf-8") as file:
     reader = csv.DictReader(file)   # read file as DICTIONARY
 
-    for row in reader:
-        students.append({"name": row["name"], "home": row["home"]})
+#     for row in reader:
+#         students.append({"name": row["name"], "home": row["home"]})
 
-for student in sorted(students, 
-                      key=lambda student: student["name"], 
-                      reverse=False):
-    print(f"{student['name']} is from {student['home']}")
+# for student in sorted(students, 
+#                       key=lambda student: student["name"], 
+#                       reverse=False):
+#     print(f"{student['name']} is from {student['home']}")
+
+    for student in sorted(reader, key=lambda s: s["name"]):
+        print(f"{student['name']} is from {student['home']}")
---
+++ 6lecture/students7.py
@@ -0,0 +1,8 @@
+import csv
+
+name = input("What's your name? ")
+home = input("What's your home? ")
+
+with open("students7.csv", "a") as file:
+    writer = csv.writer(file)
+    writer.writerow([name, home])
