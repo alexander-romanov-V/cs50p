1f30df0e890231ec665e43f967c40105255be388
alexander-romanov-V <romanovav@ya.ru>
2025-04-03 13:14:16 +0300

    5.14

--- 6lecture/students7.py
+++ 6lecture/students7.py
@@ -3,6 +3,8 @@ import csv
 name = input("What's your name? ")
 home = input("What's your home? ")
 
-with open("students7.csv", "a") as file:
-    writer = csv.writer(file)
-    writer.writerow([name, home])
+with open("students7.csv", "a", encoding="utf-8") as file:
+    # writer = csv.writer(file)
+    # writer.writerow([name, home])
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
