a1a6c269c6ede9bec22cb888712dfcbbe39d411c
alexander-romanov-V <romanovav@ya.ru>
2025-04-04 11:07:55 +0300

    5.15

---
+++ 6shorts/image.py
@@ -0,0 +1,20 @@
+"""Pillow short"""
+
+from PIL import Image
+from PIL import ImageFilter
+
+def main():
+    """Main code"""
+    # img = Image.open("in.jpeg")
+    # img.close()
+    with Image.open("in.jpeg") as img:
+        print(img.size)
+        print(img.format)
+        img = img.rotate(180)
+        # img = img.filter(ImageFilter.BLUR)
+        img = img.filter(ImageFilter.FIND_EDGES)
+        img.save("out.jpeg")
+
+
+if __name__ == "__main__":
+    main()
