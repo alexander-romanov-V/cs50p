98a62ced71b2cb292180e4968daf7b05f5d337d9
alexander-romanov-V <romanovav@ya.ru>
2025-04-10 19:31:19 +0300

    7.8

---
+++ 7labs/watch.py
@@ -0,0 +1,22 @@
+"""Exctract youtube links and returns its short version"""
+
+import re
+
+
+def main():
+    """Main code"""
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    """Exctract youtube links and returns its short version"""
+    pattern = (
+        r"<iframe.*src=\"https?://(?:www\.)?youtube\.com/embed/([\d\w]*)\".*></iframe>"
+    )
+    if match := re.search(pattern, s):
+        return "https://youtu.be/" + match.group(1)
+    return None
+
+
+if __name__ == "__main__":
+    main()
