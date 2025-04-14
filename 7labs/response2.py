def00f5be1d8372b9db21774859fdc28e5febfd9
alexander-romanov-V <romanovav@ya.ru>
2025-04-14 11:55:44 +0300

    7.17

---
+++ 7labs/response2.py
@@ -0,0 +1,22 @@
+"""Response Validation 2"""
+
+import validators
+
+# pip install validator-collection
+# or
+# pip install validators            [this code]
+
+
+def main():
+    """Main code"""
+    email = input("What's your email address? ")
+    print("Valid" if validate_email(email) else "Invalid")
+
+
+def validate_email(email):
+    """Validate email address"""
+    return email == "" or validators.email(email) is True
+
+
+if __name__ == "__main__":
+    main()
