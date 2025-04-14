316817655003d933d8e92b613d2fd291a3898b99
alexander-romanov-V <romanovav@ya.ru>
2025-04-14 13:36:04 +0300

    8.1

---
+++ 8lecture/student.py
@@ -0,0 +1,21 @@
+"""OOP Example"""
+
+def main():
+    """Main code"""
+    name = get_name()
+    house = get_house()
+    print(f"{name} from {house}")
+
+
+def get_name():
+    """Ask a name"""
+    return input("Name: ")
+
+
+def get_house():
+    """Ask a house"""
+    return input("House: ")
+
+
+if __name__ == "__main__":
+    main()
