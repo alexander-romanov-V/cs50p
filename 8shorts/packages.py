56a32a155da7e37b573336fb78d33d8f0e66c699
alexander-romanov-V <romanovav@ya.ru>
2025-04-16 15:08:21 +0300

    8.9

---
+++ 8shorts/packages.py
@@ -0,0 +1,8 @@
+"""shorts 8 - Packages"""
+
+def main():
+    """Main code"""
+    packages = ["Package1: Alice to Bob, 10kg", "Package 2: Bob to Charlie, 5kg"]
+
+if __name__ == "__mani__":
+    main()
