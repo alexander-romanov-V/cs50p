58a93aed0144e2c72a685d5195f0b6b14dd731a7
alexander-romanov-V <romanovav@ya.ru>
2025-04-16 15:13:42 +0300

    8.10

--- 8shorts/packages.py
+++ 8shorts/packages.py
@@ -1,8 +1,20 @@
 """shorts 8 - Packages"""
 
+
+class Package:
+    """Represent a package class"""
+
+    def __init__(self, number, sender, recipient, weight) -> None:
+        self.number = number
+        self.sender = sender
+        self.recipient = recipient
+        self.weight = weight
+
+
 def main():
     """Main code"""
     packages = ["Package1: Alice to Bob, 10kg", "Package 2: Bob to Charlie, 5kg"]
+
 
 if __name__ == "__mani__":
     main()
