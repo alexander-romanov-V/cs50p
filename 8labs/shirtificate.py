c7e4a19b0544c0d71fcc23e61b8e9cf3cd5ac9f1
alexander-romanov-V <romanovav@ya.ru>
2025-04-18 12:14:59 +0300

    8.20

--- 8labs/shirtificate.py
+++ 8labs/shirtificate.py
@@ -14,13 +14,13 @@ def main():
     pdf.set_auto_page_break(False)
 
     pdf.set_text_color(0, 0, 0)
-    pdf.set_font("arial", style="B", size=44)
+    pdf.set_font("helvetica", style="B", size=44)
     pdf.cell(center=True, text="CS50 Shirtificate\n", h=50)
 
     pdf.image("shirtificate.png", keep_aspect_ratio=True, x=10, y=70, w=pdf.epw)
 
     pdf.set_text_color(255, 255, 255)
-    pdf.set_font("calibri", style="B", size=22)
+    pdf.set_font("courier", style="B", size=22)
     pdf.cell(center=True, text=f"{name} took CS50", h=250)
 
     pdf.output("shirtificate.pdf")
