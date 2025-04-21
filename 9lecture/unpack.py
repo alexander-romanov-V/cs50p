19fca70a6c35597b0602f7d67d341e81e0f3c8b1
alexander-romanov-V <romanovav@ya.ru>
2025-04-21 11:40:54 +0300

    9.13

--- 9lecture/unpack.py
+++ 9lecture/unpack.py
@@ -19,5 +19,19 @@ coins = [100, 50, 25]
 # 2 - try simple
 print(total(coins[0], coins[1], coins[2]), "Knuts")
 
-# 3 - try smarter - UNPACK values
+# 3 - try smarter - UNPACK list values with *
 print(total(*coins), "Knuts")
+
+
+
+# 1.1 - source code
+print(total(galleons=100, sickles=50, knuts=25), "Knuts")
+
+# store coins in the dict
+coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+
+# 2.1 - try simple
+print(total(coins["galleons"], coins["sickles"], coins["knuts"]), "Knuts")
+
+# 3.1 - try smarter - UNPACK dic values with **
+print(total(**coins), "Knuts")
